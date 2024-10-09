7e19fad99cfdf58cc1d4dcb319926fb200e0493e
T1WiLLi <williambeaudun@gmail.com>
2024-10-09 17:43:03 -0400

    feat(entities): Added updatableEntity to allow the update of a given entity without implying that it is moving

---
+++ pps/src/main/java/pewpew/smash/engine/entities/UpdatableEntity.java
@@ -0,0 +1,7 @@
+package pewpew.smash.engine.entities;
+
+public abstract class UpdatableEntity extends StaticEntity {
+    public abstract void updateClient();
+
+    public abstract void updateServer();
+}
