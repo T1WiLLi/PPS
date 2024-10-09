bdde975c1829a4c7b6117066b83144376bcce540
T1WiLLi <williambeaudun@gmail.com>
2024-10-09 17:34:47 -0400

    feat(configuration): Added the database configuration class to securely load and dipose of the private information of the database.

---
+++ pps/src/main/java/pewpew/smash/database/configuration/DatabaseConfig.java
@@ -0,0 +1,51 @@
+package pewpew.smash.database.configuration;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
+import pewpew.smash.game.utils.ResourcesLoader;
+
+public final class DatabaseConfig {
+
+    private final static DatabaseConfig INSTANCE = new DatabaseConfig();
+
+    @Getter
+    private String HOST;
+    @Getter
+    private String USER;
+    @Getter
+    private String PASSWORD;
+
+    public static DatabaseConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public void consume() {
+        this.HOST = "N/A";
+        this.USER = "N/A";
+        this.PASSWORD = "N/A";
+    }
+
+    private DatabaseConfig() {
+        try {
+            loadConfig();
+        } catch (IOException e) {
+            consume();
+            e.printStackTrace();
+        }
+    }
+
+    private void loadConfig() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper
+                .readTree(ResourcesLoader.getMiscFile(ResourcesLoader.CONFIG_PATH, "databaseConfig.json"));
+        JsonNode databaseNode = root.path("database");
+
+        this.HOST = databaseNode.path("host").asText();
+        this.USER = databaseNode.path("user").asText();
+        this.PASSWORD = databaseNode.path("password").asText();
+    }
+}
