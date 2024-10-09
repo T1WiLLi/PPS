b5a45e324b983e13d07087a44e6191d1fefaf3be
T1WiLLi <williambeaudun@gmail.com>
2024-10-09 17:37:53 -0400

    feat(models): Added the Player model to represent the player table from the db

---
+++ pps/src/main/java/pewpew/smash/database/models/Player.java
@@ -0,0 +1,16 @@
+package pewpew.smash.database.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+    private int id;
+    private String username;
+    private String passwordHash;
+    private Rank rank;
+    private List<Achievement> achievements;
+}
