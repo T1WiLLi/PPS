38df5c6c293f5de74ed3917564dda65a6f023e72
T1WiLLi <williambeaudun@gmail.com>
2024-10-09 21:07:05 -0400

    Refactor(Ui Element): Refactor the Slider class to better adhere to SOLID principles

--- pps/src/main/java/pewpew/smash/game/ui/Slider.java
+++ pps/src/main/java/pewpew/smash/game/ui/Slider.java
@@ -30,13 +30,40 @@ public class Slider extends UiElement {
 
     public Slider(int x, int y, int width, int height, float initialValue) {
         super(x, y, width, height);
-        this.value = Math.max(0.0f, Math.min(1.0f, initialValue));
+        this.value = clampValue(initialValue);
         this.handleBounds = new Rectangle();
         updateHandlePosition();
     }
 
     @Override
     protected void loadSprites(BufferedImage spriteSheet) {
+        // No sprites to load for Slider
+    }
+
+    @Override
+    public void update() {
+        updateScaledBounds();
+        updateHandleBounds();
+        handleMouseInput();
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        renderTrack(canvas);
+        renderHandle(canvas);
+    }
+
+    public void setValue(float newValue) {
+        this.value = clampValue(newValue);
+        updateHandlePosition();
+    }
+
+    public int getHandleXStart() {
+        return ScaleUtils.scaleX(handleX);
+    }
+
+    public int getHandleXEnd() {
+        return ScaleUtils.scaleX(handleX + HANDLE_WIDTH);
     }
 
     private void updateHandlePosition() {
@@ -52,47 +79,39 @@ public class Slider extends UiElement {
                 ScaleUtils.scaleHeight(height));
     }
 
-    @Override
-    public void update() {
-        updateScaledBounds();
-        updateHandleBounds();
-
+    private void handleMouseInput() {
         int scaledMouseX = MouseController.getMouseX();
         int scaledMouseY = MouseController.getMouseY();
 
-        if (mousePressed) {
-            int scaledSliderStart = ScaleUtils.scaleX(xPos);
-            int scaledSliderEnd = ScaleUtils.scaleX(xPos + width - HANDLE_WIDTH);
-
-            int constrainedMouseX = Math.max(scaledSliderStart, Math.min(scaledMouseX, scaledSliderEnd));
-
-            float newValue = (float) (constrainedMouseX - scaledSliderStart) /
-                    (scaledSliderEnd - scaledSliderStart);
-            setValue(newValue);
-        }
-
         mouseOver = bounds.contains(scaledMouseX, scaledMouseY);
+
+        if (mousePressed) {
+            updateValueFromMouse(scaledMouseX);
+        }
     }
 
-    @Override
-    public void render(Canvas canvas) {
+    private void updateValueFromMouse(int scaledMouseX) {
+        int scaledSliderStart = ScaleUtils.scaleX(xPos);
+        int scaledSliderEnd = ScaleUtils.scaleX(xPos + width - HANDLE_WIDTH);
+
+        int constrainedMouseX = Math.max(scaledSliderStart, Math.min(scaledMouseX, scaledSliderEnd));
+
+        float newValue = (float) (constrainedMouseX - scaledSliderStart) /
+                (scaledSliderEnd - scaledSliderStart);
+        setValue(newValue);
+    }
+
+    private void renderTrack(Canvas canvas) {
         int trackY = yPos + (height - TRACK_HEIGHT) / 2;
         canvas.renderRectangleBorder(xPos, trackY, width, TRACK_HEIGHT, 3, Color.WHITE);
+    }
 
+    private void renderHandle(Canvas canvas) {
         canvas.renderRectangle(handleX, yPos, HANDLE_WIDTH, height, Color.WHITE);
         canvas.renderRectangleBorder(handleX, yPos, HANDLE_WIDTH, height, 1, Color.BLACK);
     }
 
-    public void setValue(float newValue) {
-        this.value = Math.max(0.0f, Math.min(1.0f, newValue));
-        updateHandlePosition();
-    }
-
-    public int getHandleXStart() {
-        return ScaleUtils.scaleX(handleX);
-    }
-
-    public int getHandleXEnd() {
-        return ScaleUtils.scaleX(handleX + HANDLE_WIDTH);
+    private float clampValue(float value) {
+        return Math.max(0.0f, Math.min(1.0f, value));
     }
 }
