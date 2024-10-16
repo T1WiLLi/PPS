c51519665970930eb8daa543e60f4af5369c7976
William Beaudin <williambeaudun@gmail.com>
2024-10-16 12:16:53 -0400

    refactor(Checkbox): Override and supercharge the handleMouseInput function for the checkbox so that it take a consumer task.

--- pps/src/main/java/pewpew/smash/game/ui/Checkbox.java
+++ pps/src/main/java/pewpew/smash/game/ui/Checkbox.java
@@ -1,10 +1,17 @@
 package pewpew.smash.game.ui;
 
 import pewpew.smash.engine.Canvas;
+import pewpew.smash.game.audio.AudioClip;
+import pewpew.smash.game.audio.AudioPlayer;
+import pewpew.smash.game.audio.AudioPlayer.SoundType;
+import pewpew.smash.game.utils.HelpMethods;
 import pewpew.smash.game.utils.ResourcesLoader;
 
 import java.awt.image.BufferedImage;
 import java.awt.Color;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -35,6 +42,23 @@ public class Checkbox extends UiElement {
         if (checked) {
             canvas.renderImage(checkSprite, xPos, yPos - 10, this.width + 10, this.height + 10);
         }
+    }
+
+    public void handleMouseInput(boolean isPressed, Consumer<Boolean> setter) {
+        if (HelpMethods.isIn(bounds)) {
+            checked = !checked;
+            setter.accept(checked);
+            AudioPlayer.getInstance().play(AudioClip.SWAPPED, 0.95f, false, SoundType.UI);
+        }
+    }
+
+    @Override
+    protected void handleMouseInput(boolean isPressed) {
+    }
+
+    @Override
+    public void handleMouseMove() {
+
     }
 
     @Override
