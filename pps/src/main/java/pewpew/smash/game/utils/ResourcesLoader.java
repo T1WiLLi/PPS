ded63955c97e9a8608b1af97f473e9e36580b650
William Beaudin <williambeaudun@gmail.com>
2024-10-23 14:02:07 -0400

    Refactor(ResourcesLoader): Implement a caching system for the resources to optimize and reduce memory usage.

--- pps/src/main/java/pewpew/smash/game/utils/ResourcesLoader.java
+++ pps/src/main/java/pewpew/smash/game/utils/ResourcesLoader.java
@@ -1,9 +1,12 @@
 package pewpew.smash.game.utils;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioFormat;
@@ -13,6 +16,10 @@ import javax.sound.sampled.AudioSystem;
 import pewpew.smash.game.audio.AudioData;
 
 public class ResourcesLoader {
+
+    private static final ConcurrentHashMap<String, BufferedImage> imageCache = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, AudioData> audioCache = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, byte[]> miscCache = new ConcurrentHashMap<>();
 
     private static final String BASE_PATH = "/pewpew/smash/";
     // Image
@@ -32,10 +39,18 @@ public class ResourcesLoader {
 
     public static BufferedImage getImage(String folderPath, String filename) {
         String path = folderPath + "/" + filename + ".png";
+
+        BufferedImage cachedImage = imageCache.get(path);
+        if (cachedImage != null) {
+            return cachedImage;
+        }
+
         InputStream is = ResourcesLoader.class.getResourceAsStream(path);
         if (is != null) {
             try {
-                return ImageIO.read(is);
+                BufferedImage image = ImageIO.read(is);
+                imageCache.put(path, image);
+                return image;
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -55,28 +70,50 @@ public class ResourcesLoader {
 
     public static AudioData getAudioData(String basePath, String filename) {
         String path = basePath + "/" + filename + ".wav";
+
+        AudioData cachedAudio = audioCache.get(path);
+        if (cachedAudio != null) {
+            return cachedAudio;
+        }
+
         try (InputStream is = ResourcesLoader.class.getResourceAsStream(path)) {
-            if (is == null) {
+            if (is != null) {
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(is);
+                AudioFormat format = audioStream.getFormat();
+                byte[] audioData = audioStream.readAllBytes();
+                audioStream.close();
+
+                AudioData newAudioData = new AudioData(format, audioData);
+                audioCache.put(path, newAudioData);
+                return newAudioData;
+            } else {
                 System.err.println("Audio file not found: " + path);
                 return null;
             }
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(is);
-            AudioFormat format = audioStream.getFormat();
-            byte[] audioData = audioStream.readAllBytes();
-            audioStream.close();
-            return new AudioData(format, audioData);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     public static InputStream getMiscFile(String basePath, String filename) throws FileNotFoundException {
         String path = basePath + "/" + filename;
-        InputStream inputStream = ResourcesLoader.class.getResourceAsStream(path);
-        if (inputStream == null) {
-            throw new FileNotFoundException("Resource not found: " + path);
+
+        byte[] cachedData = miscCache.get(path);
+        if (cachedData != null) {
+            return new java.io.ByteArrayInputStream(cachedData);
         }
-        return inputStream;
+
+        try (InputStream is = ResourcesLoader.class.getResourceAsStream(path)) {
+            if (is != null) {
+                byte[] data = is.readAllBytes();
+                miscCache.put(path, data);
+                return new ByteArrayInputStream(data);
+            } else {
+                throw new FileNotFoundException("Resource not found: " + path);
+            }
+        } catch (IOException e) {
+            throw new FileNotFoundException("Failed to read resource: " + path);
+        }
     }
 }
