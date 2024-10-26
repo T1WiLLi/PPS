b3df53fa6f8e30a8d588a0e7e5af2c45bdfa55c3
William Beaudin <williambeaudun@gmail.com>
2024-10-26 13:21:15 -0400

    feat(Packets): Implemented the inventory packet to share inventory at the death of a player.

--- pps/src/main/java/pewpew/smash/game/entities/Inventory.java
+++ pps/src/main/java/pewpew/smash/game/entities/Inventory.java
@@ -23,26 +23,6 @@ public class Inventory<K, V> {
         }
     }
 
-    public static class SerializedItem {
-        public enum ItemType {
-            CONSUMABLE,
-            WEAPON,
-            AMMO_STACK
-        }
-
-        public ItemType type;
-        public String itemIdentifier;
-        public int quantity;
-        public Map<String, Integer> extraData;
-
-        public SerializedItem(ItemType type, String itemIdentifier, int quantity) {
-            this.type = type;
-            this.itemIdentifier = itemIdentifier;
-            this.quantity = quantity;
-            this.extraData = new HashMap<>();
-        }
-    }
-
     public Inventory() {
         this.slots = new HashMap<>();
     }
--- pps/src/main/java/pewpew/smash/game/objects/weapon/Fist.java
+++ pps/src/main/java/pewpew/smash/game/objects/weapon/Fist.java
@@ -3,13 +3,24 @@ package pewpew.smash.game.objects.weapon;
 import java.awt.Shape;
 import java.awt.image.BufferedImage;
 
+import java.awt.Color;
+
 import pewpew.smash.engine.Canvas;
 import pewpew.smash.game.objects.MeleeWeapon;
 
 public class Fist extends MeleeWeapon {
 
+    private int leftFistX, leftFistY;
+    private int rightFistX, rightFistY;
+    private int centerX, centerY;
+    private int radius = 18;
+
+    private boolean isLeftFistAttacking = false;
+    private boolean isRightFistAttacking = false;
+
     public Fist(String name, String description, BufferedImage preview) {
         super(name, description, preview);
+        setDimensions(6, 6);
     }
 
     @Override
@@ -19,7 +30,8 @@ public class Fist extends MeleeWeapon {
 
     @Override
     public void render(Canvas canvas) {
-
+        renderFist(canvas, leftFistX, leftFistY);
+        renderFist(canvas, rightFistX, rightFistY);
     }
 
     @Override
@@ -29,11 +41,75 @@ public class Fist extends MeleeWeapon {
 
     @Override
     public void attack() {
-
+        if (isLeftFistAttacking) {
+            if (!isReturning) {
+                attackProgress += attackSpeed;
+                if (attackProgress >= 1.0f) {
+                    attackProgress = 1.0f;
+                    isReturning = true;
+                }
+            } else {
+                attackProgress -= attackSpeed;
+                if (attackProgress <= 0.0f) {
+                    attackProgress = 0.0f;
+                    isLeftFistAttacking = false;
+                    isReturning = false;
+                    isRightFistAttacking = true;
+                }
+            }
+        } else if (isRightFistAttacking) {
+            if (!isReturning) {
+                attackProgress += attackSpeed;
+                if (attackProgress >= 1.0f) {
+                    attackProgress = 1.0f;
+                    isReturning = true;
+                }
+            } else {
+                attackProgress -= attackSpeed;
+                if (attackProgress <= 0.0f) {
+                    attackProgress = 0.0f;
+                    isRightFistAttacking = false;
+                    isReturning = false;
+                    isAttacking = false;
+                }
+            }
+        }
     }
 
     @Override
     public Shape getHitbox() {
         return null;
+    }
+
+    public void updatePosition(int playerX, int playerY, float rotation) {
+        this.centerX = playerX + 10;
+        this.centerY = playerY + 10;
+
+        teleport(centerX, centerY);
+        updateFistPositions(rotation);
+    }
+
+    private void renderFist(Canvas canvas, int x, int y) {
+        int fistRadius = 8;
+        canvas.renderCircle(x, y, fistRadius, Color.BLACK);
+        canvas.renderCircle(x + 2, y + 2, fistRadius - 2, new Color(229, 194, 152));
+    }
+
+    private void updateFistPositions(float rotation) {
+        double angleRad = Math.toRadians(rotation);
+
+        double leftAngle = angleRad - Math.PI / 4;
+        double rightAngle = angleRad + Math.PI / 4;
+
+        int forwardOffset = (int) (range * attackProgress);
+
+        int forwardX = (int) (forwardOffset * Math.cos(angleRad));
+        int forwardY = (int) (forwardOffset * Math.sin(angleRad));
+
+        leftFistX = (int) (centerX + radius * Math.cos(leftAngle)) + (isLeftFistAttacking ? forwardX : 0);
+        leftFistY = (int) (centerY + radius * Math.sin(leftAngle)) + (isLeftFistAttacking ? forwardY : 0);
+
+        rightFistX = (int) (centerX + radius * Math.cos(rightAngle)) + (isRightFistAttacking ? forwardX : 0);
+        rightFistY = (int) (centerY + radius * Math.sin(rightAngle)) + (isRightFistAttacking ? forwardY : 0);
     }
 }
