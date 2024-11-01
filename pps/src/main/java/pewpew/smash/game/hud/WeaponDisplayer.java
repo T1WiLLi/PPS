1683634310e2ec3d5b26a60b1a547b69928fa28a
T1WiLLi <williambeaudun@gmail.com>
2024-11-01 05:05:22 -0400

    Feat(Inventory): Started revamp of the inventory system to make it simpler while making it better.

---
+++ pps/src/main/java/pewpew/smash/game/hud/WeaponDisplayer.java
@@ -0,0 +1,14 @@
+package pewpew.smash.game.hud;
+
+import pewpew.smash.engine.Canvas;
+
+public class WeaponDisplayer extends HudElement {
+
+    public WeaponDisplayer(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public void render(Canvas canvas) {
+
+    }
+}
