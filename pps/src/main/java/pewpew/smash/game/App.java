04b1604234395e9ac0f60f2e8255b1682fa6ed50
T1WiLLi <williambeaudun@gmail.com>
2024-11-13 13:15:05 -0500

    fix(Rendering): Fixed rendering performance by utilizing a VolatileImage and OpenGL and Direct3D perks to the JVM.

--- pps/src/main/java/pewpew/smash/game/App.java
+++ pps/src/main/java/pewpew/smash/game/App.java
@@ -2,6 +2,8 @@ package pewpew.smash.game;
 
 public class App {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        System.setProperty("sun.java2d.d3d", "true");
         Launcher.loadResources();
         new PewPewSmash().start();
     }
