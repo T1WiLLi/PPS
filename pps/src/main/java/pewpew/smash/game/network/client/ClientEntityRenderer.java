d4e800b36805690f50c2caa912068fa8c763b8ff
T1WiLLi <williambeaudun@gmail.com>
2024-11-14 15:40:00 -0500

    Feat(DeltaRendering): Implemented deltaRendering to improve performance over larger area with a lot of object.

--- pps/src/main/java/pewpew/smash/game/network/client/ClientEntityRenderer.java
+++ pps/src/main/java/pewpew/smash/game/network/client/ClientEntityRenderer.java
@@ -4,55 +4,51 @@ import pewpew.smash.engine.Canvas;
 import pewpew.smash.engine.entities.StaticEntity;
 import pewpew.smash.game.Camera;
 import pewpew.smash.game.network.manager.EntityManager;
-import pewpew.smash.game.utils.ScaleUtils;
+import pewpew.smash.game.utils.ViewUtils;
 
 public class ClientEntityRenderer {
     private final EntityManager entityManager;
-    private static final double FOV_BUFFER = 0.05; // 5% buffer for FOV calculations
-    private static final int BASE_WIDTH = 800;
-    private static final int BASE_HEIGHT = 600;
 
     public ClientEntityRenderer(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
     public void render(Canvas canvas, Camera camera) {
-        ViewBounds bounds = calculateBounds(canvas, camera);
-
-        renderPlayers(canvas, camera, bounds);
-        renderMovableEntities(canvas, camera, bounds);
-        renderUpdatableEntities(canvas, camera, bounds);
-
-        renderBulletEntities(canvas, camera, bounds);
+        renderPlayers(canvas, camera);
+        renderMovableEntities(canvas, camera);
+        renderUpdatableEntities(canvas, camera);
+        renderBulletEntities(canvas, camera);
 
         canvas.resetScale();
     }
 
-    private void renderPlayers(Canvas canvas, Camera camera, ViewBounds bounds) {
+    private void renderPlayers(Canvas canvas, Camera camera) {
         entityManager.getPlayerEntities().forEach(player -> {
-            renderEntity(canvas, camera, player);
+            if (ViewUtils.isInView(player.getX(), player.getY())) {
+                renderEntity(canvas, camera, player);
+            }
         });
     }
 
-    private void renderMovableEntities(Canvas canvas, Camera camera, ViewBounds bounds) {
+    private void renderMovableEntities(Canvas canvas, Camera camera) {
         entityManager.getMovableEntities().forEach(entity -> {
-            if (isInView(entity.getX(), entity.getY(), bounds)) {
+            if (ViewUtils.isInView(entity.getX(), entity.getY())) {
                 renderEntity(canvas, camera, entity);
             }
         });
     }
 
-    private void renderUpdatableEntities(Canvas canvas, Camera camera, ViewBounds bounds) {
+    private void renderUpdatableEntities(Canvas canvas, Camera camera) {
         entityManager.getUpdatableEntities().forEach(entity -> {
-            if (isInView(entity.getX(), entity.getY(), bounds)) {
+            if (ViewUtils.isInView(entity.getX(), entity.getY())) {
                 renderEntity(canvas, camera, entity);
             }
         });
     }
 
-    private void renderBulletEntities(Canvas canvas, Camera camera, ViewBounds bounds) {
+    private void renderBulletEntities(Canvas canvas, Camera camera) {
         entityManager.getBulletEntities().forEach(entity -> {
-            if (isInView((int) entity.getX(), (int) entity.getY(), bounds)) {
+            if (ViewUtils.isInView((int) entity.getX(), (int) entity.getY())) {
                 canvas.translate(-camera.getX(), -camera.getY());
                 entity.render(canvas);
                 canvas.translate(camera.getX(), camera.getY());
@@ -64,33 +60,5 @@ public class ClientEntityRenderer {
         canvas.translate(-camera.getX(), -camera.getY());
         entity.render(canvas);
         canvas.translate(camera.getX(), camera.getY());
-    }
-
-    private ViewBounds calculateBounds(Canvas canvas, Camera camera) {
-        double scaleX = ScaleUtils.getScaleX();
-        double scaleY = ScaleUtils.getScaleY();
-
-        double bufferX = BASE_WIDTH * FOV_BUFFER;
-        double bufferY = BASE_HEIGHT * FOV_BUFFER;
-
-        return new ViewBounds(
-                camera.getX() / scaleX - bufferX,
-                camera.getX() / scaleX + BASE_WIDTH + bufferX,
-                camera.getY() / scaleY - bufferY,
-                camera.getY() / scaleY + BASE_HEIGHT + bufferY);
-    }
-
-    private boolean isInView(int x, int y, ViewBounds bounds) {
-        double scaleX = ScaleUtils.getScaleX();
-        double scaleY = ScaleUtils.getScaleY();
-
-        double scaledX = x / scaleX;
-        double scaledY = y / scaleY;
-
-        return scaledX >= bounds.minX && scaledX <= bounds.maxX &&
-                scaledY >= bounds.minY && scaledY <= bounds.maxY;
-    }
-
-    private record ViewBounds(double minX, double maxX, double minY, double maxY) {
     }
 }
---
+++ pps/src/main/java/pewpew/smash/game/utils/ViewUtils.java
@@ -0,0 +1,40 @@
+package pewpew.smash.game.utils;
+
+import pewpew.smash.game.Camera;
+
+public class ViewUtils {
+    private static final double FOV_BUFFER = 0.05;
+    private static final int SCREEN_DEFAULT_WIDTH = 800;
+    private static final int SCREEN_DEFAULT_HEIGHT = 600;
+
+    public static boolean isInView(int x, int y) {
+        ViewBounds bounds = calculateBounds();
+
+        double scaleX = ScaleUtils.getScaleX();
+        double scaleY = ScaleUtils.getScaleY();
+
+        double scaledX = x / scaleX;
+        double scaledY = y / scaleY;
+
+        return scaledX >= bounds.minX && scaledX <= bounds.maxX &&
+                scaledY >= bounds.minY && scaledY <= bounds.maxY;
+    }
+
+    private static ViewBounds calculateBounds() {
+        double scaleX = ScaleUtils.getScaleX();
+        double scaleY = ScaleUtils.getScaleY();
+        double zoom = Camera.getZoom();
+
+        double bufferX = (SCREEN_DEFAULT_WIDTH / zoom) * FOV_BUFFER;
+        double bufferY = (SCREEN_DEFAULT_HEIGHT / zoom) * FOV_BUFFER;
+
+        return new ViewBounds(
+                Camera.getInstance().getX() / scaleX - bufferX,
+                Camera.getInstance().getX() / scaleX + (SCREEN_DEFAULT_WIDTH / zoom) + bufferX,
+                Camera.getInstance().getY() / scaleY - bufferY,
+                Camera.getInstance().getY() / scaleY + (SCREEN_DEFAULT_HEIGHT / zoom) + bufferY);
+    }
+
+    public static record ViewBounds(double minX, double maxX, double minY, double maxY) {
+    }
+}
