d1151c24d4931c4a92f4c88a0d061ae48de3d136
T1WiLLi <williambeaudun@gmail.com>
2024-12-03 11:09:22 -0500

    fix(Issue #116): Balanced the weapon and reduce overall damage output.

--- pps/src/main/java/pewpew/smash/game/objects/WeaponType.java
+++ pps/src/main/java/pewpew/smash/game/objects/WeaponType.java
@@ -6,16 +6,24 @@ import lombok.Getter;
 
 @Getter
 public enum WeaponType {
+    // Pistols
+    GLOCK(8, 400, 0.3, Optional.of(1), Optional.of(1.5), Optional.of(15)),
+    COLT45(15, 500, 0.4, Optional.of(2), Optional.of(1.8), Optional.of(7)),
+    DEAGLE(20, 600, 0.5, Optional.of(2), Optional.of(2.2), Optional.of(7)),
 
-    FIST(10, 25, 0.020, Optional.empty(), Optional.empty(), Optional.empty()),
-    AK47(35, 2500, 0.1, Optional.of(3), Optional.of(2.5), Optional.of(30)),
-    HK416(38, 2700, 0.1, Optional.of(4), Optional.of(2.3), Optional.of(30)),
-    M1A1(45, 3200, 0.4, Optional.of(4), Optional.of(3.0), Optional.of(20)),
-    MAC10(25, 800, 0.08, Optional.of(2), Optional.of(2.1), Optional.of(32)),
-    MP5(30, 1200, 0.09, Optional.of(2), Optional.of(2.2), Optional.of(25)),
-    COLT45(50, 600, 0.6, Optional.of(1), Optional.of(1.8), Optional.of(7)),
-    DEAGLE(75, 750, 0.7, Optional.of(2), Optional.of(2.5), Optional.of(7)),
-    GLOCK(20, 800, 0.4, Optional.of(2), Optional.of(2.0), Optional.of(15));
+    // SMG's
+    MAC10(10, 800, 0.08, Optional.of(3), Optional.of(2.0), Optional.of(32)),
+    MP5(12, 1000, 0.09, Optional.of(3), Optional.of(2.1), Optional.of(25)),
+
+    // Assaut Rifles
+    AK47(14, 2500, 0.12, Optional.of(3), Optional.of(2.5), Optional.of(30)),
+    HK416(16, 2700, 0.11, Optional.of(4), Optional.of(2.3), Optional.of(30)),
+
+    // Precision Rifles
+    M1A1(18, 3200, 0.2, Optional.of(4), Optional.of(3.0), Optional.of(20)),
+
+    // Fists by default.
+    FIST(5, 25, 0.5, Optional.empty(), Optional.empty(), Optional.empty());
 
     private final int damage;
     private final int range;
